25148649585421a7ef904ce116eb306c192d9568
oluseunakin <akindoyinoluseun@gmail.com>
2023-01-25 20:33:38 +0100

    yy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.end('welcome')
+    console.log("in other")
+    //res.end('welcome')
 })
 
 server.on('request', (req, res) => {
+    console.log("in request")
     res.writeHead(200).end('worked')
 })
 
