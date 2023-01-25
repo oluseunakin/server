224fe517ba180061ef404cdd0a0696da0556f505
oluseunakin <akindoyinoluseun@gmail.com>
2023-01-25 20:06:33 +0100

    jjhj

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const server = http.createServer((req, res) => {
     res.end('welcome')
 })
 
+server.on('request', (req, res) => {
+    res.writeHead(200).end('worked')
+})
+
 server.listen(9000, () => {
     console.log('Server started')
 })
