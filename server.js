1dfe46594398ec87420d5040fadf157e0f45b7d9
oluseunakin <akindoyinoluseun@gmail.com>
2023-04-24 17:05:36 +0100

    ddd

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('hi')
+const http = require('http')
+
+const server = http.createServer(3000, (req, res) => {
+    res.end('hello from server')
+})
+
+server.listen(3000, 'thisserver', () => {
+    console.log('server is up')
+})
